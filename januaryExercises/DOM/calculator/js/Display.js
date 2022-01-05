8c97b5bd7cc5635752ec1b6807b8cb060b7f5791
Fernando BG <ferbg9@gmail.com>
2022-01-05 13:44:24 +0100

    commit

--- januaryExercises/DOM/calculator/js/Display.js
+++ januaryExercises/DOM/calculator/js/Display.js
@@ -1,12 +1,25 @@
 class Display {
     constructor(displayPreviousValue, displayCurrentValue) {
-        this.displayPreviousValue = displayPreviousValue;
         this.displayCurrentValue = displayCurrentValue;
+        this.displayPreviousValue = displayPreviousValue;
         this.calculator = new Calculator();
-        this.previousValue = '';
         this.currentValue = '';
+        this.previousValue = '';
     }
+
+    erase() {
+        this.currentValue = this.currentValue.toString().slice(0,-1);
+        this.printValues();
+    }
+
     attachNumber(number) {
-        this.currentValue = number;
+        if(number === '.' && this.currentValue.includes('.')) return
+        this.currentValue = this.currentValue.toString() + number.toString();
+        this.printValues();
+    }
+
+    printValues() {
+        this.displayCurrentValue.textContent = this.currentValue;
+        this.displayPreviousValue.textContent = this.previousValue;
     }
 }
--- januaryExercises/DOM/calculator/js/script.js
+++ januaryExercises/DOM/calculator/js/script.js
@@ -1,9 +1,10 @@
-const displayPreviousValue = document.getElementById('displayPreviousValue');
-const displayCurrentValue = document.getElementById('displayCurrentValue');
+const displayPreviousValue = document.getElementById('previous-value');
+const displayCurrentValue = document.getElementById('current-value');
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 
-const display = new Display(displayPreviousValue, displayCurrentValue)
+const display = new Display(displayPreviousValue, displayCurrentValue);
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => display.attachNumber(button.innerHTML));
 });
