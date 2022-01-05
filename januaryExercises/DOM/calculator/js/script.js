497de8432f7d1aa8d725bb300860559d81532b37
Fernando BG <ferbg9@gmail.com>
2022-01-05 12:11:20 +0100

    testing add, substract, multiply and divide

--- januaryExercises/DOM/calculator/js/script.js
+++ januaryExercises/DOM/calculator/js/script.js
@@ -4,3 +4,7 @@ const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 
 const calculator = new Calculator;
+console.log(calculator.add(2,3));
+console.log(calculator.substract(2,3));
+console.log(calculator.multiply(2,3));
+console.log(calculator.divide(2,3));
