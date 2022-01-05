7f64c987171372a319baa6dab7247124be80dac7
Fernando BG <ferbg9@gmail.com>
2022-01-05 12:10:34 +0100

    Calculator class created

---
+++ januaryExercises/DOM/calculator/js/script.js
@@ -0,0 +1,6 @@
+const previousValue = document.getElementById('previousValue');
+const currentValue = document.getElementById('currentValue');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+const calculator = new Calculator;
